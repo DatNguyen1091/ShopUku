4c3e238b71ca091ee19740715bde9fc96ef9d793
DatNguyen1091 <nvdat0204@gmail.com>
2023-07-30 12:19:44 +0700

    API CartItem

---
+++ ShopUku/ShopUku-BAL/Services/CartItemService.cs
@@ -0,0 +1,41 @@
+﻿using ShopUku_DAL.Model;
+using ShopUku_DAL.Repository;
+
+namespace ShopUku_BAL.Services
+{
+    public class CartItemService
+    {
+        private CartItemRepository _cartItemRepository;
+        public CartItemService(CartItemRepository cartItemRepository)
+        {
+            _cartItemRepository = cartItemRepository;
+        }
+
+        public List<CartItems> GetAllCartItems(int? page)
+        {
+            return _cartItemRepository.GetAll(page);
+        }
+
+        public CartItems GetCartItemsById(int id)
+        {
+            return _cartItemRepository.GetById(id);
+        }
+
+        public CartItems CreateCartItems(CartItems model)
+        {
+            return _cartItemRepository.Create(model);
+        }
+
+        public CartItems UpdateCartItems(int id, CartItems model)
+        {
+            var result = _cartItemRepository.Update(id, model);
+            return result;
+        }
+
+        public string RemoveCartItems(int id)
+        {
+            var result = _cartItemRepository.Delete(id);
+            return result;
+        }
+    }
+}
---
+++ ShopUku/ShopUku/Controllers/CartItemController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopUku_BAL.Services;
+using ShopUku_DAL.Model;
+
+namespace ShopUku.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartItemController : ControllerBase
+    {
+        private readonly CartItemService _cartItemService;
+        public CartItemController(CartItemService cartItemService)
+        {
+            _cartItemService = cartItemService;
+        }
+
+        [HttpGet]
+        public List<CartItems> GetCartItems(int? page)
+        {
+            return _cartItemService.GetAllCartItems(page);
+        }
+
+        [HttpGet("{id}")]
+        public CartItems GetCartItemsId(int id)
+        {
+            return _cartItemService.GetCartItemsById(id);
+        }
+
+        [HttpPost]
+        public CartItems PostCartItems(CartItems model)
+        {
+            return _cartItemService.CreateCartItems(model);
+        }
+
+        [HttpPut("{id}")]
+        public CartItems PutCartItems(int id, CartItems model)
+        {
+            return _cartItemService.UpdateCartItems(id, model);
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteCartItems(int id)
+        {
+            return _cartItemService.RemoveCartItems(id);
+        }
+    }
+}
