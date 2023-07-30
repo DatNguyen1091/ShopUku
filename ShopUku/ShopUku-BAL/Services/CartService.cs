7183fe30064d0a579d517ed0f7c251c6bf00c013
DatNguyen1091 <nvdat0204@gmail.com>
2023-07-30 12:30:00 +0700

    API Carts

---
+++ ShopUku/ShopUku-BAL/Services/CartService.cs
@@ -0,0 +1,42 @@
+﻿using ShopUku_DAL.Model;
+using ShopUku_DAL.Repository;
+
+namespace ShopUku_BAL.Services
+{
+    public class CartService
+    {
+        private CartRepository _cartRepository;
+
+        public CartService(CartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        public List<Carts> GetAllCarts(int? page)
+        {
+            return _cartRepository.GetAll(page);
+        }
+
+        public Carts GetCartsById(int id)
+        {
+            return _cartRepository.GetById(id);
+        }
+
+        public Carts CreateCarts(Carts model)
+        {
+            return _cartRepository.Create(model);
+        }
+
+        public Carts UpdateCarts(int id, Carts model)
+        {
+            var result = _cartRepository.Update(id, model);
+            return result;
+        }
+
+        public string RemoveCarts(int id)
+        {
+            var result = _cartRepository.Delete(id);
+            return result;
+        }
+    }
+}
---
+++ ShopUku/ShopUku/Controllers/CartsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopUku_BAL.Services;
+using ShopUku_DAL.Model;
+
+namespace ShopUku.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartsController : ControllerBase
+    {
+        private readonly CartService _cartService;
+
+        public CartsController(CartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [HttpGet]
+        public List<Carts> GetCarts(int? page)
+        {
+            return _cartService.GetAllCarts(page);
+        }
+
+        [HttpGet("{id}")]
+        public Carts GetCartsId(int id)
+        {
+            return _cartService.GetCartsById(id);
+        }
+
+        [HttpPost]
+        public Carts PostCarts(Carts model)
+        {
+            return _cartService.CreateCarts(model);
+        }
+
+        [HttpPut("{id}")]
+        public Carts PutCarts(int id, Carts model)
+        {
+            return _cartService.UpdateCarts(id, model);
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteCarts(int id)
+        {
+            return _cartService.RemoveCarts(id);
+        }
+    }
+}
