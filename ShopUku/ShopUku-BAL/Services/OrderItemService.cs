b3c40019db1b0e7c8fe6c1b5affe9798d5863065
DatNguyen1091 <nvdat0204@gmail.com>
2023-07-30 13:11:54 +0700

    API OrderItems

---
+++ ShopUku/ShopUku-BAL/Services/OrderItemService.cs
@@ -0,0 +1,42 @@
+﻿using ShopUku_DAL.Model;
+using ShopUku_DAL.Repository;
+
+namespace ShopUku_BAL.Services
+{
+    public class OrderItemService
+    {
+        private OrderItemRepository _orderItemRepository;
+
+        public OrderItemService(OrderItemRepository orderItemRepository)
+        {
+            _orderItemRepository = orderItemRepository;
+        }
+
+        public List<OrderItems> GetAllOrderItems(int? page)
+        {
+            return _orderItemRepository.GetAll(page);
+        }
+
+        public OrderItems GetOrderItemsById(int id)
+        {
+            return _orderItemRepository.GetById(id);
+        }
+
+        public OrderItems CreateOrderItems(OrderItems model)
+        {
+            return _orderItemRepository.Create(model);
+        }
+
+        public OrderItems UpdateOrderItems(int id, OrderItems model)
+        {
+            var result = _orderItemRepository.Update(id, model);
+            return result;
+        }
+
+        public string RemoveOrderItems(int id)
+        {
+            var result = _orderItemRepository.Delete(id);
+            return result;
+        }
+    }
+}
---
+++ ShopUku/ShopUku/Controllers/OrderItemController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopUku_BAL.Services;
+using ShopUku_DAL.Model;
+
+namespace ShopUku.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderItemController : ControllerBase
+    {
+        private readonly OrderItemService _orderItemService;
+
+        public OrderItemController(OrderItemService orderItemService)
+        {
+            _orderItemService = orderItemService;
+        }
+
+        [HttpGet]
+        public List<OrderItems> GetOrderItems(int? page)
+        {
+            return _orderItemService.GetAllOrderItems(page);
+        }
+
+        [HttpGet("{id}")]
+        public OrderItems GetOrderItemsId(int id)
+        {
+            return _orderItemService.GetOrderItemsById(id);
+        }
+
+        [HttpPost]
+        public OrderItems PostOrderItems(OrderItems model)
+        {
+            return _orderItemService.CreateOrderItems(model);
+        }
+
+        [HttpPut("{id}")]
+        public OrderItems PutOrderItems(int id, OrderItems model)
+        {
+            return _orderItemService.UpdateOrderItems(id, model);
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteOrderItems(int id)
+        {
+            return _orderItemService.RemoveOrderItems(id);
+        }
+    }
+}
