af326f8342fc56d0367c8a864900e6b40fd27840
DatNguyen1091 <nvdat0204@gmail.com>
2023-07-30 13:17:06 +0700

    API Orders

---
+++ ShopUku/ShopUku-BAL/Services/OrderService.cs
@@ -0,0 +1,42 @@
+﻿using ShopUku_DAL.Model;
+using ShopUku_DAL.Repository;
+
+namespace ShopUku_BAL.Services
+{
+    public class OrderService
+    {
+        private OrderRepository _orderRepository;
+
+        public OrderService(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public List<Orders> GetAllOrder(int? page)
+        {
+            return _orderRepository.GetAll(page);
+        }
+
+        public Orders GetOrderById(int id)
+        {
+            return _orderRepository.GetById(id);
+        }
+
+        public Orders CreateOrder(Orders model)
+        {
+            return _orderRepository.Create(model);
+        }
+
+        public Orders UpdateOrder(int id, Orders model)
+        {
+            var result = _orderRepository.Update(id, model);
+            return result;
+        }
+
+        public string RemoveOrder(int id)
+        {
+            var result = _orderRepository.Delete(id);
+            return result;
+        }
+    }
+}
---
+++ ShopUku/ShopUku/Controllers/OrdersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopUku_BAL.Services;
+using ShopUku_DAL.Model;
+
+namespace ShopUku.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrderService _orderService;
+
+        public OrdersController(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public List<Orders> GetOrder(int? page)
+        {
+            return _orderService.GetAllOrder(page);
+        }
+
+        [HttpGet("{id}")]
+        public Orders GetOrderId(int id)
+        {
+            return _orderService.GetOrderById(id);
+        }
+
+        [HttpPost]
+        public Orders PostOrder(Orders model)
+        {
+            return _orderService.CreateOrder(model);
+        }
+
+        [HttpPut("{id}")]
+        public Orders PutOrder(int id, Orders model)
+        {
+            return _orderService.UpdateOrder(id, model);
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteOrder(int id)
+        {
+            return _orderService.RemoveOrder(id);
+        }
+    }
+}
