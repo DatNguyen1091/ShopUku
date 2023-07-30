9a6bcde4d79c4df9f78906f37d5ab209e2bc5fab
DatNguyen1091 <nvdat0204@gmail.com>
2023-07-30 12:11:37 +0700

    Add model

---
+++ ShopUku/ShopUku-DAL/Model/Carts.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ShopUku_DAL.Model
+{
+    public class Carts
+    {
+        public int id { get; set; }
+        public string? uniqueCartId { get; set; }
+        public string? cartStatus { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+    }
+}
